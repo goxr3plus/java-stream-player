f3307f7fcaa3dcd78293d5e42a4521a0ec850f9f
Helge Stenström <h.stenstrom@gmail.com>
2019-08-29 23:12:14 +0200

    Some unit tests
    In the TimeTool test, I think I've found a bug. Check lines 45 and 46.
    
    The tests are written to document the current behavior, rather than to guard against bugs.
    
    The time duration test don't test very much. It's because the method needs input data that I can't easily create in the test. With test-driven development, the method would have been written in a different way.

---
+++ src/test/java/com/goxr3plus/streamplayer/stream/StreamPlayerEventTest.java
@@ -0,0 +1,57 @@
+package com.goxr3plus.streamplayer.stream;
+
+import com.goxr3plus.streamplayer.enums.Status;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class StreamPlayerEventTest {
+
+    private Object source;
+    private Object description;
+    private Status status;
+    private int encodededStreamPosition;
+    private StreamPlayerEvent event;
+
+    @BeforeEach
+    void setUp() {
+        description = new Object();
+        source = new Object();
+        status = Status.RESUMED;
+        encodededStreamPosition = 12345;
+        event = new StreamPlayerEvent(source, status, encodededStreamPosition, description);
+    }
+
+    @Test
+    void itReturnsTheStatus() {
+        assertEquals(status, event.getPlayerStatus());
+    }
+
+    @Test
+    void itReturnsTheEncodedStreamPosition() {
+        assertEquals(encodededStreamPosition, event.getEncodedStreamPosition());
+    }
+
+    @Test
+    void itReturnsTheSource() {
+        assertSame(source, event.getSource());
+    }
+
+    @Test
+    void itReturnsTheDescription() {
+        assertSame(description, event.getDescription());
+    }
+
+    @Test
+    void itReturnsAString() {
+        final String actual = event.toString();
+        final String expected = "Source :="
+                + source.toString()
+                + " , Player Status := RESUMED , EncodedStreamPosition :=12345 , Description :="
+                + description.toString();
+        assertEquals(expected, actual);
+    }
+
+
+}
