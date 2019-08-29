17e4da3bbc30436f176a64d230d62490f10e911b
Helge Stenström <h.stenstrom@gmail.com>
2019-08-29 21:31:58 +0200

    Another main, application and listener implementation
    This implementation doesn't subclass StreamPlayer, and it doesn't implement the StreamPlayerListener interface. Instead, the application has an instance of the regular StreamPlayer, and there is a separate implementation of the StreamPlayerListener interface.
    
    One thing I dislike, is that the StreamPlayerListener depends on the StreamPlayer; it imports it, and has an instance of it, because that is how it gets the total number of bytes.
    
    I have also added an alternative progress logging message.
    
    I would have prefered to create the listener i main(), but it needs information that only the application has (the filename), so it has to be created in the application.
    
    Besides that, it works as usual.

---
+++ src/main/java/com/goxr3plus/streamplayer/application/AnotherDemoApplication.java
@@ -0,0 +1,74 @@
+package com.goxr3plus.streamplayer.application;
+
+import com.goxr3plus.streamplayer.enums.Status;
+import com.goxr3plus.streamplayer.stream.StreamPlayer;
+import com.goxr3plus.streamplayer.stream.StreamPlayerEvent;
+import com.goxr3plus.streamplayer.stream.StreamPlayerListener;
+
+import java.io.File;
+import java.util.Map;
+
+/**
+ * @author GOXR3PLUS
+ *
+ */
+public class AnotherDemoApplication  {
+
+	private final String audioFileName = "Logic - Ballin [Bass Boosted].mp3";
+
+	private StreamPlayer streamPlayer;
+	private StreamPlayerListener listener;
+
+	public AnotherDemoApplication(StreamPlayer streamPlayer) {
+		this.streamPlayer = streamPlayer;
+		this.listener = new AnotherStreamPlayerListener(audioFileName, streamPlayer);
+
+	}
+
+
+	void start() {
+		try {
+
+			// Register to the Listeners
+		 	streamPlayer.addStreamPlayerListener(listener);
+
+			// Open a File
+			// open(new File("...")) //..Here must be the file absolute path
+			// open(INPUTSTREAM)
+			// open(AUDIOURL)
+
+			// Example
+			streamPlayer.open(new File(audioFileName));
+
+			//Seek by bytes
+			//seekBytes(500000L);
+
+			//Seek +x seconds starting from the current position
+			streamPlayer.seekSeconds(15); // forward 15 seconds
+			streamPlayer.seekSeconds(15); // forward 15 seconds again
+
+			/* Seek starting from the begginning of the audio */
+			//seekTo(200);
+
+			// Play it
+			streamPlayer.play();
+			//pause();
+
+		} catch (final Exception ex) {
+			ex.printStackTrace();
+		}
+	}
+
+
+
+
+	private String getExtension(String audioFileName) {
+		return audioFileName.split("\\.(?=[^.]+$)")[1];
+	}
+
+
+//	public static void main(final String[] args) {
+//		new AnotherDemoApplication();
+//	}
+
+}
