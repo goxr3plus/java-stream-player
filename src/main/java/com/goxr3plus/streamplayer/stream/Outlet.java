db4c7135c8fb8b2a23d92347f2f714e7245fce71
Helge Stenström <h.stenstrom@gmail.com>
2019-09-10 08:19:26 +0200

    Update Outlet.java
    Alphabetically sorted methods

--- src/main/java/com/goxr3plus/streamplayer/stream/Outlet.java
+++ src/main/java/com/goxr3plus/streamplayer/stream/Outlet.java
@@ -10,49 +10,46 @@ public class Outlet {
     private FloatControl gainControl;
     private BooleanControl muteControl;
     private FloatControl panControl;
-
-    /** The source data line. */
     private SourceDataLine sourceDataLine;
 
-    public SourceDataLine getSourceDataLine() {
-        return sourceDataLine;
-    }
-
-    public void setSourceDataLine(SourceDataLine sourceDataLine) {
-        this.sourceDataLine = sourceDataLine;
-    }
-
-
-    public BooleanControl getMuteControl() {
-        return muteControl;
-    }
-
-    public void setMuteControl(BooleanControl muteControl) {
-        this.muteControl = muteControl;
-    }
-
-
-    public FloatControl getPanControl() {
-        return panControl;
-    }
-
-    public void setPanControl(FloatControl panControl) {
-        this.panControl = panControl;
-    }
 
     public FloatControl getBalanceControl() {
         return balanceControl;
-    }
-
-    public void setBalanceControl(FloatControl balanceControl) {
-        this.balanceControl = balanceControl;
     }
 
     public FloatControl getGainControl() {
         return gainControl;
     }
 
+    public BooleanControl getMuteControl() {
+        return muteControl;
+    }
+
+    public FloatControl getPanControl() {
+        return panControl;
+    }
+
+    public SourceDataLine getSourceDataLine() {
+        return sourceDataLine;
+    }
+
+    public void setBalanceControl(FloatControl balanceControl) {
+        this.balanceControl = balanceControl;
+    }
+
     public void setGainControl(FloatControl gainControl) {
         this.gainControl = gainControl;
+    }
+
+    public void setMuteControl(BooleanControl muteControl) {
+        this.muteControl = muteControl;
+    }
+
+    public void setPanControl(FloatControl panControl) {
+        this.panControl = panControl;
+    }
+
+    public void setSourceDataLine(SourceDataLine sourceDataLine) {
+        this.sourceDataLine = sourceDataLine;
     }
 }
