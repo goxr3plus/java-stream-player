6d71851b1b413a3c2c676c96ba49a3c21a486fb6
Helge Stenström <h.stenstrom@gmail.com>
2019-09-14 23:58:08 +0200

    getAudioFileFormat pushed down to sub classes

--- src/main/java/com/goxr3plus/streamplayer/stream/FileDataSource.java
+++ src/main/java/com/goxr3plus/streamplayer/stream/FileDataSource.java
@@ -1,18 +1,22 @@
 package com.goxr3plus.streamplayer.stream;
 
 import javax.sound.sampled.AudioFileFormat;
+import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.UnsupportedAudioFileException;
 import java.io.File;
 import java.io.IOException;
 
 public class FileDataSource extends DataSource {
 
+    File source;
+
     FileDataSource(File source) {
         super(source);
+        this.source = source;
     }
 
     @Override
     public AudioFileFormat getAudioFileFormat() throws UnsupportedAudioFileException, IOException {
-        return super.getAudioFileFormat();
+        return AudioSystem.getAudioFileFormat(this.source);
     }
 }
--- src/main/java/com/goxr3plus/streamplayer/stream/StreamDataSource.java
+++ src/main/java/com/goxr3plus/streamplayer/stream/StreamDataSource.java
@@ -1,18 +1,22 @@
 package com.goxr3plus.streamplayer.stream;
 
 import javax.sound.sampled.AudioFileFormat;
+import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.UnsupportedAudioFileException;
 import java.io.IOException;
 import java.io.InputStream;
 
 public class StreamDataSource extends DataSource {
 
+    InputStream source;
+
     StreamDataSource(InputStream source) {
         super(source);
+        this.source = source;
     }
 
     @Override
     public AudioFileFormat getAudioFileFormat() throws UnsupportedAudioFileException, IOException {
-        return super.getAudioFileFormat();
+        return AudioSystem.getAudioFileFormat(source);
     }
 }
